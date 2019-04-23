bc651d6c3ab88287eb9546258e5146c6f57c7496
Song Gao <song@gao.io>
2019-04-22 18:26:17 -0700

    implement folder view filter as designed

--- shared/fs/common/folder-view-filter-icon.js
+++ shared/fs/common/folder-view-filter-icon.js
@@ -1,39 +1,32 @@
 // @flow
 import {namedConnect} from '../../util/container'
-import * as Platforms from '../../constants/platform'
 import * as Types from '../../constants/types/fs'
 import * as Constants from '../../constants/fs'
 import * as Kb from '../../common-adapters'
-import * as FsGen from '../../actions/fs-gen'
 import * as Styles from '../../styles'
 import * as React from 'react'
-import {debounce} from 'lodash-es'
 import flags from '../../util/feature-flags'
 
 type Props = {|
-  onBlur?: ?() => void,
-  onUpdate: string => void,
+  onClick: () => void,
   path: Types.Path,
   pathItem: Types.PathItem,
   style?: ?Styles.StylesCrossPlatform,
 |}
 
-const FolderViewFilter = (props: Props) =>
+export const FolderViewFilterIcon = (props: Props) =>
   Constants.isFolder(props.path, props.pathItem) &&
   Types.getPathLevel(props.path) > 1 && (
-    <Kb.SearchFilter
-      focusOnMount={Styles.isMobile}
-      hotkey="f"
-      onBlur={props.onBlur}
-      onChange={props.onUpdate}
-      placeholderText="Filter"
-      type={Platforms.isMobile ? 'Mobile' : 'Small'}
-      style={props.style}
+    <Kb.Icon
+      type="iconfont-filter"
+      onClick={props.onClick}
+      padding="tiny"
+      style={props.style && Kb.iconCastPlatformStyles(props.style)}
     />
   )
 
 type OwnProps = {|
-  onBlur?: ?() => void,
+  onClick: () => void,
   path: Types.Path,
   style?: ?Styles.StylesCrossPlatform,
 |}
@@ -41,13 +34,10 @@ type OwnProps = {|
 const mapStateToProps = (state, {path}) => ({
   pathItem: state.fs.pathItems.get(path, Constants.unknownPathItem),
 })
-const mapDispatchToProps = (dispatch, {path}: OwnProps) => ({
-  _onUpdate: (newFilter: string) => dispatch(FsGen.createSetFolderViewFilter({filter: newFilter})),
-})
+const mapDispatchToProps = dispatch => ({})
 
 const mergeProps = (s, d, o) => ({
-  onBlur: o.onBlur,
-  onUpdate: debounce(d._onUpdate, 100),
+  onClick: o.onClick,
   path: o.path,
   pathItem: s.pathItem,
   style: o.style,
@@ -55,6 +45,9 @@ const mergeProps = (s, d, o) => ({
 
 export default (!flags.folderViewFilter
   ? () => null
-  : namedConnect<OwnProps, _, _, _, _>(mapStateToProps, mapDispatchToProps, mergeProps, 'FolderViewFilter')(
-      FolderViewFilter
-    ))
+  : namedConnect<OwnProps, _, _, _, _>(
+      mapStateToProps,
+      mapDispatchToProps,
+      mergeProps,
+      'FolderViewFilterIcon'
+    )(FolderViewFilterIcon))
